53648c5b37abc17a15274de6acae423aaeef3609
Lou Mayans <loumayans@hotmail.com>
2019-04-07 16:04:42 -0400

    Added barebone ability to register and login

--- Flask/flaskr/__init__.py
+++ Flask/flaskr/__init__.py
@@ -1,8 +1,10 @@
 import os
 
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 from flask_bootstrap import Bootstrap
+from flaskr import db
 
+database = None
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -10,6 +12,12 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    #Create database 
+    database = db.database(app)
+    #for i in database.Query():
+        #print(i['Name'])
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -32,14 +40,31 @@ def create_app(test_config=None):
         return render_template("landing.html")
 
     #Login page 
-    @app.route('/login')
+    @app.route('/login', methods=['GET', 'POST'])
     def Login():
+        if request.method == "POST":
+            print("Post Login")
+            loggedIn = database.CheckIfUserInDB(str(request.form['username']),str(request.form['password']))
+            if loggedIn == True:
+                return render_template("events.html")
+
         return render_template("login.html")
 
     #Registration page 
-    @app.route('/register')
+    @app.route('/register', methods=['GET', 'POST'])
     def Register():
-        return render_template("register.html")
+        stringy = "false"
+        if request.method == "POST":
+            stringy = "true"
+            result = database.AddUser(str(request.form['username']),str(request.form['password']))
+            if result != None:
+                for i in result:
+                    for j in i:
+                        print(j)
+
+            print("POST REGISTER")
+            return render_template("register.html",stringy = stringy)
+        return render_template("register.html",stringy = stringy)
 
     #Profile for creator 
     @app.route('/creator')
--- Flask/flaskr/db.py
+++ Flask/flaskr/db.py
@@ -3,18 +3,72 @@ import sshtunnel
 #https://www.lfd.uci.edu/~gohlke/pythonlibs/#mysqlclient
 #pip install mysqlclient-1.4.2-cp37-cp37m-win32.whl
 
-db = None
-tunnel = None
+#https://kite.com/python/docs/sqlalchemy.engine.result.ResultProxy
 
-def DBStart(app):
-	tunnel = sshtunnel.SSHTunnelForwarder(
-		('ssh.pythonanywhere.com'), ssh_username="flimsyware",ssh_password="flimsythefish",
-		remote_bind_address=('flimsyware.mysql.pythonanywhere-services.com',3306)
-	)
-	tunnel.start()
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flimsyware:flimsydatabase@127.0.0.1:{}/flimsyware$default'.format(tunnel.local_bind_port)
-	db = SQLAlchemy(app)
+class database:
+	def __init__(self,app):
+		self.tunnel = sshtunnel.SSHTunnelForwarder(
+			('ssh.pythonanywhere.com'), ssh_username="flimsyware",ssh_password="flimsythefish",
+			remote_bind_address=('flimsyware.mysql.pythonanywhere-services.com',3306)
+		)
+		self.tunnel.start()
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flimsyware:flimsydatabase@127.0.0.1:{}/flimsyware$default'.format(self.tunnel.local_bind_port)
+		self.db = SQLAlchemy(app)
+
+	def Query(self):
+		return self.db.engine.execute("SELECT * FROM University WHERE Name = \"Akron\";")
+
+	def AddUser(self,email,password, firstName = "John", lastName = "Doe",Role = "Guest",UniversityID = 0):
+		if issubclass(type(email), str) == False:
+			print("email not type of str.")
+			return None
+			
+		if issubclass(type(password), str) == False:
+			print("password not type of str.")
+			return None
+
+		if issubclass(type(firstName), str) == False:
+			print("firstName not type of str.")
+			return None
+
+		if issubclass(type(lastName), str) == False:
+			print("lastName not type of str.")
+			return None
+
+		if issubclass(type(Role), str) == False:
+			print("Role not type of str.")
+			return None
+
+		if issubclass(type(UniversityID), int) == False:
+			print("UniversityID not type of str.")
+			return None
+
+		queryText = "INSERT INTO User (Email,Password,FirstName,LastName,Role,UniversityID) "\
+			"values (\"" + email + "\",\"" + password + "\",\"" + firstName + "\",\"" + lastName + "\",\"" + Role + "\",\"" + str(UniversityID) +"\");"
+		result = self.db.engine.execute(queryText)
+
+	def CheckIfUserInDB(self, email, password):
+		if issubclass(type(email), str) == False:
+			print("email not type of str.")
+			return False
+		
+		if issubclass(type(password), str) == False:
+			print("password not type of str.")
+			return False
+
+		queryText = "select * from User where (Email,Password) = (\"" + email + "\",\"" + password + "\");"
+		result = self.db.engine.execute(queryText)
+		rows = result.fetchall()
+
+		for i in rows:
+			print(i)
+
+		if len(rows) > 0:
+			print("YES")
+			return True
+		else:
+			print("NO")
+			return False
 
 
-def Query():
-	return db.engine.execute("SELECT * FROM University WHERE Name = \"Akron\";")
+		
