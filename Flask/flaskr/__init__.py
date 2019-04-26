a62baffe719aad8cf42a49ea308917022943b01e
ibedard16 <brokenLogician@gmail.com>
2019-04-26 14:30:08 -0400

    Move Database connection string to configuration This makes it possible to create a special "mock database" for testing. I'll get tests running next commit, before this commit gets too dense.

--- Flask/flaskr/__init__.py
+++ Flask/flaskr/__init__.py
@@ -10,11 +10,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join('database.db'),
     )
-
-    #Database
-    dbHelper = DBHelper()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -22,6 +19,10 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+
+    #Database
+    with app.app_context():
+        dbHelper = DBHelper()
 
     # ensure the instance folder exists
     try:
